cac23e56a9f1017439a932eb6ce6d8e63df786fc
GeethanjaliVuruma <83065709+GeethanjaliVuruma@users.noreply.github.com>
2021-08-02 15:51:40 +0530

    Add files via upload

---
+++ Code.js
@@ -0,0 +1,25 @@
+const accessCode1 = "VARIABLE";
+const accessCode2 = "FUNCTION";
+const accessCode3 = "DATABASE";
+
+function clues() {
+    
+    fill("white")
+    textSize(15)
+    text("R E V B A I L A", 100,50)
+    fill("lightblue")
+    text("Hint: Always changing, not constant !!", 100,70)
+// add code for displaying rest of the names and the hints.
+fill("white")
+textSize(15)
+text("U C I N T O F N", 400,100)
+fill("lightblue")
+text("Hint: Also called method!!", 400,70)
+
+fill("white")
+textSize(15)
+text("D T B E A S A A", 700,50)
+fill("lightblue")
+text("Hint:HELPS IN SYNC OF GAME!!", 700,70)
+
+}
